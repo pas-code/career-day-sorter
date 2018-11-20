0617bce28494d3245fa7ac7ca67a642dc440c1e4
Joshua Kent <kentjo@pascack.org>
2018-11-20 10:20:49 -0500

    create priority class & CareerLogicTester

---
+++ src/com/atcs/career/data/Priority.java
@@ -0,0 +1,39 @@
+//Joshua Kent
+//Program description:
+//Nov 20, 2018
+
+package com.atcs.career.data;
+
+public class Priority
+{
+   private double cMag, tMag, gMag;
+   private double cWeight, tWeight, gWeight;
+   
+   public Priority(double cMag, double tMag, double gMag, double cWeight, double tWeight, double gWeight)
+   {
+      this.cMag = cMag;
+      this.tMag = tMag;
+      this.gMag = gMag;
+      this.cWeight = cWeight;
+      this.tWeight = tWeight;
+      this.gWeight = gWeight;
+   }
+   
+   public double getContentness(){
+      return cMag*cWeight;
+   }
+   
+   public double getTime(){
+      return tMag*tWeight;
+   }
+   
+   public double getGroup(){
+      return gMag*gWeight;
+   }
+   
+   public double getPriority(){
+      return getContentness() + getTime() + getGroup();
+   }
+
+   
+}
