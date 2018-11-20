1dc6d4bc4eb01f5041863b7bf30f346fbf7de689
Joshua Kent <kentjo@pascack.org>
2018-11-20 09:55:46 -0500

    open logic branch

---
+++ src/com/atcs/career/logic/CareerLogicTester.java
@@ -0,0 +1,12 @@
+//Joshua Kent
+//Program description:
+//Nov 20, 2018
+
+package com.atcs.career.logic;
+
+public class CareerLogicTester
+{
+   public static void main(String[] args)
+   {
+   }
+}
