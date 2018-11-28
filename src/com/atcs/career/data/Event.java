910b2f4263ca2668e7011e95d88ff2fce9917abb
mreineke <michaelrein528@gmail.com>
2018-11-28 14:50:48 -0500

    implement Serializable to Event class

--- src/com/atcs/career/data/Event.java
+++ src/com/atcs/career/data/Event.java
@@ -3,17 +3,32 @@
 
 package com.atcs.career.data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Event{
-	private int amountOfSessions;
-	private ArrayList<Session> sessions;
-	private static ArrayList<Student> students = new ArrayList<Student>();
-	private ArrayList<Room> rooms;
+import com.atcs.career.io.IOUtilities;
+
+public class Event implements Serializable{
+	/**
+    * 
+    */
+   private static final long serialVersionUID = -7463051683970561540L;
+   private int amountOfSessions;
+	private ArrayList<Session> sessions = new ArrayList<Session>();
+	private ArrayList<Student> students = new ArrayList<Student>();
+	private ArrayList<Room> rooms = new ArrayList<Room>();
 	
-	public static void main(String[] args)
-   {
+	/**
+	 * Creates a new Event from scratch
+	 */
+	public Event(){
+	   students = IOUtilities.loadStudentArray(IOUtilities.importCSV());
+	   //rooms =
+	   //sessions =
+	}
+	
+	public void save(){
 	   
-   }
+	}
 	
 }
