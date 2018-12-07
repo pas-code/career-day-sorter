a42ee014be118ff35dee3fdf08f937cd1afc958b
Michael Ruberto <rubertmi@pascack.org>
2018-12-07 09:40:24 -0500

    Implement contentness logic to compare requests vs assignments

--- src/com/atcs/career/logic/Algorithms.java
+++ src/com/atcs/career/logic/Algorithms.java
@@ -1,6 +1,11 @@
 //Michael Ruberto
 //Program Description:
 //Nov 21, 2018
+/*TODO
+ * - Rerank contentness between rounds
+ * - Implement ability to randomly assign sessions to students who didn't answer/couldn't get in a selected session
+ */
+
 
 package com.atcs.career.logic;
 
@@ -20,6 +25,7 @@ public class Algorithms{
     * */
    static ArrayList<ArrayList<Student>> toBeRandomlyAssigned = new ArrayList<ArrayList<Student>>();
    
+   //ALGORITHM 1
    public static void assignRoomsToSessions(ArrayList<Student> students, ArrayList<Room> rooms, ArrayList<Session> sessions){
       
       Collections.sort(rooms);
@@ -47,6 +53,7 @@ public class Algorithms{
       }
    }
    
+   //ALGORITHM 2
    public static void rankStudents(ArrayList<Student> students, int classCutOffForGroupLevel, int upperClassmanLevelMag, int lowerClassmanLevelMag){
       for(int i = 0; i < students.size(); i++) {
          Student currentStud = students.get(i);
@@ -61,6 +68,12 @@ public class Algorithms{
       Collections.sort(students);
    }
    
+   //ALGORITHM 2.5
+   public static void rerankStudents() {
+      
+   }
+   
+   //ALGORITHM 3
    public static void assignStudentsToSessions(ArrayList<Student> students, ArrayList<Session> sessions){
       //Creates Array Lists for Random Assignment
       for(int i = 0; i < 3; i++) { //Change 3 later to not be a magic number
@@ -73,6 +86,7 @@ public class Algorithms{
             Student currentStud = students.get(i); //Makes it easier to refer to current students
             assignBasedOnChoice(currentStud, sessions, j);
          }
+         //ADD RERANK STUDENTS METHOD
       } 
       
       //Assign randoms
@@ -82,13 +96,16 @@ public class Algorithms{
    public static void assignBasedOnChoice(Student currentStud, ArrayList<Session> sessions, int period) {
       for(int k = 0; k < currentStud.getRequests().size(); k++){ //Check every request the student makes
          Session desiredSession = sessions.get(findIndexOfSession(currentStud.getRequests().get(k), sessions));
-         if(desiredSession.getStudents().get(period).size() < desiredSession.getRoom().getMaxCapacity()){
+         if(desiredSession.getStudents().get(period).size() < desiredSession.getRoom().getMaxCapacity() &&
+           !currentStud.getAssignments().contains(desiredSession)){
             desiredSession.getStudents().get(period).add(currentStud);
-            currentStud.getAssignments().set(period - 1, desiredSession);      
-            currentStud.getRequests().remove(k); //Remove the session so it doesnt try and double-add them
+            currentStud.getAssignments().set(period - 1, desiredSession);
+            changeStudentContentness(currentStud); //Deals with contentness
             return;
          }
       }
+      
+      //They couldn't get in any session they chose this period
       currentStud.getAssignments().set(period - 1, null); //PLACEHOLDER LINE UNTIL RANDOM STUFF WORKS
       toBeRandomlyAssigned.get(period - 1).add(currentStud);
       
@@ -97,9 +114,18 @@ public class Algorithms{
    }
    
    public static void changeStudentContentness(Student currentStud){
+      int selectionsAlreadyMade = currentStud.getAssignments().size();
       
-      //do things
+      double numerator = 0;
+      double denominator = 0;
+      for(int i = 0; i < selectionsAlreadyMade; i++) {
+         int choiceIndex = currentStud.getRequests().indexOf(currentStud.getAssignments().get(i));
+         if(choiceIndex != -1)
+            numerator += currentStud.getRequests().size() - choiceIndex;
+         denominator += currentStud.getRequests().size() - i;
+      }
       
+      currentStud.getStudentPriority().setContentness(numerator/denominator);
    }
    
    public static int findIndexOfSession(Session requestedSession, ArrayList<Session> sessions){
